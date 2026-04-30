cffbeefc7de2993269a6dcd4acc6f0c1a84d8099
Govind <govindsuryawanshi001@gmail.com>
2026-04-30 12:21:41 +0530

    Add mobile responsive layout bug fixed for mobile

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
@@ -11,6 +11,7 @@ import { AuthService } from '../../core/services/auth.service';
   styleUrl: './sidebar.scss'
 })
 export class SidebarComponent implements OnInit {
+  @Input() isOpen = false;
   userName = signal('');
   userRole = signal('');
   currentPath = signal('');
