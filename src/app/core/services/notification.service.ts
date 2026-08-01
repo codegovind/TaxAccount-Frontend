de6e4ae8c154158ff8d2ba78081a149d2ba6fa6f
Govind Tekale <119581534+codegovind@users.noreply.github.com>
2026-08-01 20:26:06 +0530

    feat(ui): add NotificationService (MatSnackBar wrapper)

---
+++ src/app/core/services/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) { }
+
+  success(message: string, action: string = 'Close', duration = 3000) {
+    this.snackBar.open(message, action, { duration, panelClass: ['snackbar-success'] });
+  }
+
+  error(message: string, action: string = 'Close', duration = 5000) {
+    this.snackBar.open(message, action, { duration, panelClass: ['snackbar-error'] });
+  }
+
+  info(message: string, action: string = 'Close', duration = 3000) {
+    this.snackBar.open(message, action, { duration, panelClass: ['snackbar-info'] });
+  }
+}
